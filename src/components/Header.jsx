91fe000fc52c1f04e0b5008089738532d2e5951d
Harsha Prajapati <harsha.prajapati@scripbox.com>
2021-11-21 17:00:11 +0530

    Structure organised

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+
+    return(
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly'}}>
+            <div>
+                Logo Arts
+            </div>
+            <div>
+                Art Gallery
+            </div>
+            <div>
+                Details
+            </div>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Main = () => {
+
+    return(
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+         Main
+        </div>
+    )
+}
+
+export default Main;
